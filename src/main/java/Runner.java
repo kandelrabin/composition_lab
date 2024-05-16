b251705fb89acd7f900bfd979ac839c51ed4592a
Gellila Zeru <gellilazeru1@gmail.com>
2024-05-16 16:40:10 +0100

    finished MVP

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,15 +2,35 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        IEngine hondaEngine = new CombustionEngine(99, "01/01/2023");
-        Car jazz = new Car(1100.0f, 160, "hatchback", hondaEngine);
+//        IEngine hondaEngine = new CombustionEngine(99, "01/01/2023");
+//        Car jazz = new Car(1100.0f, 160, "hatchback", hondaEngine);
+//
+//        System.out.println(jazz.getHorsePower());
+//
+//        IEngine electricEngine = new ElectricEngine(1000, 100);
+//        jazz.setEngine(electricEngine);
+//
+//        System.out.println(jazz.getHorsePower());
 
-        System.out.println(jazz.getHorsePower());
+        IEngine yamaha = new CombustionEngine(99, "16/05/2024");
+        Speedboat bnta = new Speedboat(1000f,200, true, yamaha);
+        IEngine propulsion = new CombustionEngine(1000, "14/03/1912");
+        CargoShip titanic = new CargoShip(165_000_000f, 100, 37, propulsion);
 
-        IEngine electricEngine = new ElectricEngine(1000, 100);
-        jazz.setEngine(electricEngine);
+        System.out.println(bnta.getHullType());
+        System.out.println(bnta.getHorsePower());
+        System.out.println(titanic.getHullType());
+        System.out.println(titanic.getHorsePower());
 
-        System.out.println(jazz.getHorsePower());
+        System.out.println("------");
+
+        IEngine electricEngine = new ElectricEngine(150, 80);
+        bnta.setEngine(electricEngine);
+
+        System.out.println(String.format("Speedboat has %s horse power and its Hull Type is %s.", bnta.getHorsePower(), bnta.getHullType()));
+
+
+
 
     }
 
