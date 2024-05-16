ffb4fdf3adc4ee55f0a7aed827c9f98955285d06
Rabin Kandel <kandelrabin@live.com>
2024-05-16 16:06:03 +0100

    Complete second and third mvp

--- src/main/java/CargoShip.java
+++ src/main/java/CargoShip.java
@@ -1,10 +1,14 @@
-public class CargoShip extends Vehicle {
+public class CargoShip extends Vehicle implements IWaterVehicle, IEngine{
 
     private int numberOfContainers;
+    private String hullType;
+    private IEngine engine;
 
-    public CargoShip(float weight, int maxSpeed, int horsePower, String hullType, int numberOfContainers){
-        super(weight, maxSpeed, horsePower, "steel");
+    public CargoShip(float weight, int maxSpeed, int numberOfContainers, IEngine engine){
+        super(weight, maxSpeed);
         this.numberOfContainers = numberOfContainers;
+        this.hullType = "Steel";
+        this.engine = engine;
     }
 
     public int getNumberOfContainers() {
@@ -14,4 +18,33 @@ public class CargoShip extends Vehicle {
     public void setNumberOfContainers(int numberOfContainers) {
         this.numberOfContainers = numberOfContainers;
     }
+
+    public String getHullType(){
+        return this.hullType;
+    }
+
+    public void setHullType(String hullType){
+        this.hullType = hullType;
+    }
+
+    // getter for engine property
+    public IEngine getEngine(){
+        return this.engine;
+    }
+
+    // setter for engine property
+    public void setEngine(IEngine engine){
+        this.engine = engine;
+    }
+
+    // asking engine to get horsepower
+    public int getHorsePower(){
+        return this.engine.getHorsePower();
+    }
+
+    // asking engine to set horsepower
+    public void setHorsePower(int horsePower){
+        this.engine.setHorsePower(horsePower);
+    }
+
 }
--- src/main/java/Speedboat.java
+++ src/main/java/Speedboat.java
@@ -1,18 +1,46 @@
-public class Speedboat extends Vehicle {
+public class Speedboat extends Vehicle implements IWaterVehicle, IEngine{
 
     private boolean needsCrew;
+    private String hullType;
+    private IEngine engine;
 
-    public Speedboat(float weight, int maxSpeed, int horsePower, boolean needsCrew){
-        super(weight, maxSpeed, horsePower, "fiberglass");
+    public Speedboat(float weight, int maxSpeed, boolean needsCrew, IEngine engine){
+        super(weight, maxSpeed);
         this.needsCrew = needsCrew;
+        this.hullType = "fiberglass";
+        this.engine = engine;
     }
 
     public boolean getNeedsCrew() {
-        return needsCrew;
+        return this.needsCrew;
     }
 
     public void setNeedsCrew(boolean needsCrew) {
         this.needsCrew = needsCrew;
+    }
+
+    public String getHullType(){
+        return this.hullType;
+    }
+
+    public void setHullType(String hullType){
+        this.hullType = hullType;
+    }
+
+    public IEngine getEngine(){
+        return this.engine;
+    }
+
+    public void setEngine(IEngine engine){
+        this.engine = engine;
+    }
+
+    public int getHorsePower(){
+        return this.engine.getHorsePower();
+    }
+
+    public void setHorsePower(int horsePower){
+        this.engine.setHorsePower(horsePower);
     }
 
 }
